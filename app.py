f65dfdf1aa946a053274202c63e6b4a5af6a947d
codloverS2 <mirelle.bueno21@hotmail.com>
2019-03-29 20:09:14 -0300

    first commit

--- app.py
+++ app.py
@@ -4,13 +4,12 @@ Created on Fri Mar 29 19:34:13 2019
 
 @author: Mirellebueno
 """
-
 from flask import Flask
-
-app=Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'OK!'
-if __name__=="__main__":
+app = Flask(__name__)
+ 
+@app.route("/")
+def hello():
+    return "Hello World!"
+ 
+if __name__ == "__main__":
     app.run()
