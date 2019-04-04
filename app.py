881dcf3e810855cbe4b8cf29ded74b0d07e43083
codloverS2 <mirelle.bueno21@hotmail.com>
2019-04-04 00:35:48 -0300

    updates

--- app.py
+++ app.py
@@ -148,6 +148,6 @@ def index():
 
 l = StdOutListener()
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host="0.0.0.0", port=port, debug=True) # <host_ip_address> -- replace it with the IP address of your server where you are hosting 
+#if __name__ == '__main__':
+    #port = int(os.environ.get('PORT', 5000))
+    #socketio.run(app, host="0.0.0.0", port=port, debug=True) # <host_ip_address> -- replace it with the IP address of your server where you are hosting 
